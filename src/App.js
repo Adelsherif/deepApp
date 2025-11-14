45274680b51b5528b4dae532f31a61d49e8a3305
Adel Sherif <adeladeladel515151@gmail.com>
2025-11-14 15:01:53 +0200

    deploy setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ShapPage from "./ShapPage";
 
 function App() {
